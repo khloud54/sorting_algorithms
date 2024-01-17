d4bea255e99313be45149e2c6010bba64dd4c76b
khloud54 <kh.t705042000@gmail.com>
2024-01-17 10:09:07 -0800

    del

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,43 @@
 #include "sort.h"
 
 /**
- * selection_sort - function that sorts an array of integers
- * in ascending order using modified selection sort algorithm
- *
- * @array: The array of integers to be sorted
- * @size: the number of elements in the array.
+ * modified_swap_ints - swap two integers in an array using a different approach
+ * @a: the first integer to swap
+ * @b: the second integer to swap
+*/
+void modified_swap_ints(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+/**
+ * selection_sort -  function that sorts an array of integers in ascending order
+ * @array: An array of integers
+ * @size: the size of the array
+ * Description: prints the array after each swap.
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k;
-	int temp;
+	int *min;
+	size_t i, j;
 
-	if (!array || !size)
+	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j - size - 1, k - i + 1; j > i; j--)
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			if (array[j] < array[k])
-			{
-				k = j;
-			}
-		}
-		if (array[i] > array[k])
-		{
-			temp = array[i];
-			array[i] = array[k];
-			array[k] = temp;
+			*min = *min + array[i];
+			array[i] = *min - array[i];
+			*min = *min - array[i];
+
 			print_array(array, size);
 		}
 	}
