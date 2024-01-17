69c3859321c22ba394b99e4fb9b03d4b7cafe8f3
khloud54 <kh.t705042000@gmail.com>
2024-01-17 05:30:43 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,29 +7,23 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t idx, len;
-	int temp;
-	bool has_swapped = true;
+	 size_t i, j;
+	 int temp;
 
-	if (!array || size < 2)
-		return;
+	 if (size < 2)
+		 return;
 
-	len = size;
-	while (has_swapped)
+	 for (i = 0; i <  size; i++)
 	{
-		has_swapped = false;
-		for (idx = 1; idx < len; idx++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[idx - 1] > array[idx])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[idx - 1];
-				array[idx - 1] = array[idx];
-				array[idx] = temp;
-				has_swapped = true;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
-		len--;
-
 	}
 }
